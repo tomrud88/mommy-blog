a0058ce8a1c45e66f1bff5947147b2af65d61e68
Tomasz Rudnicki <tomek198821@wp.pl>
2024-10-15 16:27:45 +0100

    first commit

---
+++ src/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from "./navbar.module.css"
+import Link from 'next/link'
+import Image from 'next/image';
+import AuthLinks from '@/AuthLinks/AuthLinks';
+
+const Navbar = () => {
+  return (
+      <div className={styles.container}>
+          <div className={styles.logo}>
+              <Image src="/logo.png" width={90} height={90} alt='logo'></Image>
+          </div>
+      <div className={styles.title}>Mama z Wyobraźnią</div>
+      <div className={styles.links}>
+        <Link href="/"  className={styles.link}>Strona Główna</Link>
+        <Link href="/"  className={styles.link}>O mnie</Link>
+        <Link href="/"  className={styles.link}>Dziennik Mamy</Link>
+        <AuthLinks/>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
+
+
---
+++ src/postList/PostList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./postList.module.css";
+
+const PostList = () => {
+  return (
+    <div>PostList</div>
+  )
+}
+
+export default PostList
