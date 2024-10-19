c71ffbf5860363c1dfa5e8626b3d93bec0455019
Tomasz Rudnicki <tomek198821@wp.pl>
2024-10-19 17:52:26 +0100

    footer and pagination layout added

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
 import styles from "./footer.module.css";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.insideContainer}>
+        <div className={styles.info}>
+          <div className={styles.logo}>
+            <Image src="/logoN.png" alt="logo" width={50} height={50} className={styles.imgLogo} />
+            <h1 className={styles.logoText}>Mama z Wyobraźnią</h1>
+          </div>
+          <p className={styles.desc}>
+            Mama z Wyobraźnią to strona internetowa stworzona z myślą o mamach,
+            które szukają inspiracji i praktycznych porad na co dzień.
+            Znajdziesz tu kreatywne pomysły na zabawy z dziećmi, sprawdzone
+            porady dotyczące rodzicielstwa, zdrowia i rozwoju dziecka, a także
+            inspiracje na ciekawe i twórcze spędzanie czasu z rodziną.
+          </p>
+          <div className={styles.icons}>
+            <Image
+              src="/communication.png"
+              alt="facebook"
+              width={18}
+              height={18}
+            />
+            <Image
+              src="/instagram.png"
+              alt="instagram"
+              width={18}
+              height={18}
+            />
+            <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+          </div>
+        </div>
+        <div className={styles.links}>
+          <div className={styles.list}>
+            <span className={styles.listTitle}>Menu</span>
+            <Link href="/home">Strona Główna</Link>
+            <Link href="/about">O mnie</Link>
+            <Link href="/mom">Dziennik Mamy</Link>
+            <Link href="/contact">Kontakt</Link>
+          </div>
+          <div className={styles.list}>
+            <span className={styles.listTitle}>Kategorie</span>
+            <Link href="/">Mama</Link>
+            <Link href="/">Zabawy</Link>
+            <Link href="/">Ksiazki</Link>
+            <Link href="/">Gry</Link>
+          </div>
+          <div className={styles.list}>
+            <span className={styles.listTitle}>Social</span>
+            <Link href="/">Facebook</Link>
+            <Link href="/">Instagram</Link>
+            <Link href="/">TikTok</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer
--- src/pagination/Pagination.jsx
+++ src/pagination/Pagination.jsx
@@ -3,8 +3,11 @@ import styles from "./pagination.module.css";
 
 const Pagination = () => {
   return (
-    <div>Pagination</div>
-  )
+    <div className={styles.container}>
+      <button className={styles.button}>Wstecz</button>
+      <button className={styles.button}>Do przodu</button>
+    </div>
+  );
 }
 
 export default Pagination
